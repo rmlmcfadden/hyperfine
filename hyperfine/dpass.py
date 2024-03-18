c4cff68bbfaa4b3672dd0d825861c0ce7046e87b
Ryan M. L. McFadden <rmlmcfadden@gmail.com>
2024-03-18 15:52:03 -0300

    updates to the dpass module
    - add module docstring
    - provide full interface for creating job files
    - improve the function documentation

--- hyperfine/dpass.py
+++ hyperfine/dpass.py
@@ -1,3 +1,17 @@
+"""Utilities for creating and running DPASS input files.
+
+DPASS (``DPASS.exe``) is a Windows program that acts as a frontend for
+(electronic) stopping cross section data calculated using the PASS program.
+It produces tabulated output from apre-calculated database of PASS outputs
+(``DPASS_DB.dat``), either through a GUI or a command line (batch) interface.
+
+For further details, see:
+
+https://www.sdu.dk/da/dpass
+
+https://doi.org/10.1016/j.nimb.2018.10.047
+"""
+
 import configparser
 import os
 
@@ -7,45 +21,72 @@ def create_job_file(
     Z_target: int,
     print_header: int = 1,
     output_units: int = 0,
-    target_mass: str = "",
-    target_density: str = "",
-    raw_data: bool = True,
+    target_mass: None | float = None,
+    target_density: None | float = None,
+    raw_data: int = 1,
+    energy_min: float = 0.001,
+    energy_max: float = 1000.0,
+    energy_points: int = 1000,
+    mesh: int = 1,
 ) -> None:
-    """
-    Create a new job file (DPASS.job) in the current directory.
+    """Create a new job file (``DPASS.job``) in the current directory.
+
+    Args:
+        Z_projectile: Atomic number of the projectile atom.
+        Z_target: Atomic number of the target atom.
+        print_header: Print header lines in ``DPASS.out`` (0 = no header; 1 = print header).
+        output_units: Units for the stopping cross sections (0 = 10^-15 eV cm^2; 1 = MeV cm^2 mg^-1; 2 = eV nm^-1).
+        target_mass: Target atomic mass [for ``output_units`` = 1 or 2] (u).
+        target_density: Target density [for ``output_units`` = 2] (g cm^-3).
+        raw_data: Use tabulated or spline interpolated points (0 =  spline interpolated points; 1 = raw tabulated points).
+        energy_min: Minimum projectile energy [for ``raw_data`` = 0] (MeV u^-1).
+        energy_max: Maximum projectile energy [for ``raw_data`` = 0] (MeV u^-1).
+        energy_points: Number of energy points [for ``raw_data`` = 0].
+        mesh: Sampling mesh for energy points [for ``raw_data = 0] (0 = linear; 1 = logarithmic).
     """
 
-    # check that the Zs are sensible
+    # check that the inputs are sensible
     assert (Z_projectile > 0) & (Z_projectile <= 92)
     assert (Z_target > 0) & (Z_target <= 92)
+    assert energy_min >= 0.001
+    assert energy_max <= 1000.0
 
     # https://docs.python.org/3/library/configparser.html
     config = configparser.ConfigParser()
     config["Main"] = {
-        "Projectile": "%d" % Z_projectile,
-        "Target": "%d" % Z_target,
+        "Projectile": f"{Z_projectile}",
+        "Target": f"{Z_target}",
     }
     config["Options"] = {
-        "Header": "%d" % print_header,
-        "Units": "%d" % output_units,
-        "A": target_mass,
-        "Rho": target_density,
+        "Header": f"{print_header}",
+        "Units": f"{output_units}",
+        "A": f"{target_mass}",
+        "Rho": f"{target_density}",
     }
     config["Spline"] = {
-        "Raw": "%d" % raw_data,
-        "Emin": "0.001",
-        "Emax": "1000.0",
-        "Epts": "1000",
-        "Log": "1",
+        "Raw": f"{raw_data}",
+        "Emin": f"{energy_min}",
+        "Emax": f"{energy_max}",
+        "Epts": f"{energy_points}",
+        "Log": f"{mesh}",
     }
     # write the config to a file
     with open("DPASS.job", "w") as jobfile:
         config.write(jobfile)
 
 
-def run_dpass(path: str = "", batch_mode: bool = True) -> int:
-    """
-    Run DPASS (in batch mode) on Linux using Wine.
+def run_dpass(
+    path: str = "",
+    batch_mode: bool = True,
+) -> int:
+    """Run DPASS (in batch mode) on Linux using Wine.
+
+    Args:
+        path: Location of ``DPASS.exe`` and ``DPASS_DB.dat``.
+        batch_mode: Flag to run DPASS in batch mode.
+
+    Returns:
+        The return code from DPASS (0 = no error; nonzero indicates abnormal termination).
     """
 
     # move to the location of DPASS.exe and DPASS_DB.dat

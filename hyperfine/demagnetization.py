41394eab46e1c6927e8f6efa83ad80d25bc6fa2e
Ryan M. L. McFadden <rmlmcfadden@gmail.com>
2024-03-18 16:08:02 -0300

    better documentation for the demagnetization module

--- hyperfine/demagnetization.py
+++ hyperfine/demagnetization.py
@@ -1,10 +1,14 @@
+"""Calculate demagnetization factors for simple geometries.
+"""
+
 import numpy as np
 from scipy import integrate
 
 
 def N_ellipsoid(a: float, b: float, c: float) -> float:
-    """
-    Demagnetization factor N for a general ellipsoid with dimensions:
+    """Demagnetization factor N for a general ellipsoid.
+
+    Calculate the demagnetization factor for an ellipsoid with dimensions:
     2a x 2b x 2c (field applied parallel to the c-axis).
 
     Calculated using numeric integration according to Eqs. (20) and (21) in:
@@ -13,6 +17,15 @@ def N_ellipsoid(a: float, b: float, c: float) -> float:
     "Effective demagnetizing factors of diamagnetic samples of various shapes",
     Phys. Rev. Appl. 10, 014030 (2018).
     https://doi.org/10.1103/PhysRevApplied.10.014030
+
+
+    Args:
+        a: Ellipsoid semi-axis.
+        b: Ellipsoid semi-axis.
+        c: Ellipsoid semi-axis.
+
+    Returns:
+        The demagnetization factor N.
     """
 
     # ensure that the semi-axes have physically meaningful dimensions
@@ -29,7 +42,7 @@ def N_ellipsoid(a: float, b: float, c: float) -> float:
 
     # perform the numeric integration using adaptive Gaussian quadrature
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.quad.html
-    result = integrate.quad(
+    result, _ = integrate.quad(
         integrand,
         0.0,  # lower integration limit
         np.inf,  # upper integration limit
@@ -42,14 +55,17 @@ def N_ellipsoid(a: float, b: float, c: float) -> float:
     prefactor = 0.5 * (b / a) * (c / a)
 
     # return the demagnetizing factor
-    return prefactor * result[0]
+    return prefactor * result
 
 
 def N_cuboid(a: float, b: float, c: float) -> float:
-    """
-    Effective demagnetizing factor for a rectangular cuboid.
+    """Effective demagnetizing factor for a rectangular cuboid.
 
-    Approximate analytic formula that interpolates between the limiting cases of an infinitely thin (c -> 0, N -> 1) and an infinitely thick (c -> +inf, N -> 0) sample (field applied parallel to the c-axis).
+    Calculate the (effective) demagnetization factor N for a rectangular cuboid
+    using an approximate analytic formula that interpolates between the
+    limiting cases of an infinitely thin (c -> 0, N -> 1) and an infinitely
+    thick (c -> +inf, N -> 0) sample. The field is assumed to be applied
+    parallel to the c-axis.
 
     Calculated using Eq. (22) from:
 
@@ -57,6 +73,14 @@ def N_cuboid(a: float, b: float, c: float) -> float:
     "Effective demagnetizing factors of diamagnetic samples of various shapes",
     Phys. Rev. Appl. 10, 014030 (2018).
     https://doi.org/10.1103/PhysRevApplied.10.014030
+
+    Args:
+        a: Length of the rectangular cuboid.
+        b: Width of the rectangular cuboid.
+        c: Height of the rectangular cuboid.
+
+    Returns:
+        The demagnetization factor N.
     """
 
     # ensure that the semi-axes have physically meaningful dimensions

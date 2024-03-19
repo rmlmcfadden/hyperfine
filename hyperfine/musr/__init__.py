d287435e24f6033d88eb2b0d2a5fb2957f7e8029
Ryan M. L. McFadden <rmlmcfadden@gmail.com>
2024-03-19 17:32:56 -0300

    improve docs for musr asymmetry functions

--- hyperfine/musr/__init__.py
+++ hyperfine/musr/__init__.py
@@ -1,9 +1,12 @@
+"""Muon spin spectroscopy (μSR) utilities.
+"""
+
 from . import meissner, _meissner
 import numpy as np
 from scipy import constants
 
 
-# muon gyromagnetic ratio in μs^-1 G^-1 (as defined in musrfit)
+#: The muon gyromagnetic ratio (μs\ :sup:`-1` G\ :sup:`-1`), as defined in musrfit.
 gamma_mu = (
     2.0
     * np.abs(
@@ -17,11 +20,31 @@ gamma_mu = (
 def raw_asymmetry(
     L: float,
     R: float,
-    l: float = 0,
-    r: float = 0,
+    l: float = 0.0,
+    r: float = 0.0,
 ) -> float:
-    """
-    Definition of asymmetry between two detectors (L)eft and (R)ight.
+    r"""Raw asymmetry between two detectors.
+
+    For a pair of detectors (L)eft and (R)ight, calculate their asymmetry:
+    .. math:
+       A_{\mathrm{raw}} = \frac{(L - l) - (R - r)}{(L - l) + (R - r)}
+
+    where
+    :math:`A_{\mathrm{raw}}` is the raw asymmetry,
+    :math:`L` is the left detector's total counts,
+    :math:`l` is the left detector's background counts,
+    :math:`R` is the right detector's total counts,
+    and
+    :math:`r` is the right detector's background counts.
+
+    Args:
+        L: Left detector total counts.
+        R: Right detector total counts.
+        l: Left detector background counts.
+        r: Right detector background counts.
+
+    Returns:
+        The raw asymmetry.
     """
 
     # do the background correction
@@ -37,13 +60,30 @@ def raw_asymmetry_error(
     L_error: float,
     R: float,
     R_error: float,
-    l: float = 0,
-    l_error: float = 0,
-    r: float = 0,
-    r_error: float = 0,
+    l: float = 0.0,
+    l_error: float = 0.0,
+    r: float = 0.0,
+    r_error: float = 0.0,
 ) -> float:
-    """
-    Error propagation of the asymmetry
+    """Uncertainty in the raw asymmetry between two detectors.
+
+    Calculate the uncertainty in the raw asymmetry using standard error
+    propagation.
+
+    Note: the calculation assumes all uncertainties are uncorrelated.
+
+    Args:
+        L: Left detector total counts.
+        L_error: Left detector uncertainty.
+        R: Right detector total counts.
+        R_error: Right detector uncertainty.
+        l: Left detector background counts.
+        l_error: Left detector background uncertainty.
+        r: Right detector background counts.
+        r_error: Right detector background uncertainty.
+
+    Returns:
+        The uncertainty of the raw asymmetry.
     """
 
     # partial derivatives
@@ -64,15 +104,49 @@ def raw_asymmetry_error(
 def corrected_asymmetry(
     L: float,
     R: float,
-    l: float = 0,
-    r: float = 0,
-    alpha: float = 1,
-    beta: float = 1,
+    l: float = 0.0,
+    r: float = 0.0,
+    alpha: float = 1.0,
+    beta: float = 1.0,
 ) -> float:
-    """
-    Definition of corrected asymmetry.
+    r"""Corrected asymmetry between two detectors.
+
+    For a pair of detectors (L)eft and (R)ight, calculate their corrected
+    asymmetry:
+    .. math:
+       A_{\mathrm{corr.}} = \frac{(\alpha - l) + (\alpha + 1) A_{\mathrm{raw}}}{(\alpha \beta + l) + (\alpha \beta - 1) A_{\mathrm{raw}}}
+
+    where
+    :math:`A_{\mathrm{corr.}}` is the corrected asymmetry,
+    :math:`\alpha` is ratio of detector counts,
+    :math:`\beta` is the ratio of detector asymmetries,
+    and
+    :math:`A_{\mathrm{raw}}` is the raw asymmetry
+
+    .. math:
+       A_{\mathrm{raw}} = \frac{(L - l) - (R - r)}{(L - l) + (R - r)}
+
+    where
+    :math:`A_{\mathrm{raw}}` is the raw asymmetry,
+    :math:`L` is the left detector's total counts,
+    :math:`l` is the left detector's background counts,
+    :math:`R` is the right detector's total counts,
+    and
+    :math:`r` is the right detector's background counts.
 
     See e.g., Noakes et al., Phys. Rev. B 35, 6597 (1987).
+    https://doi.org/10.1103/PhysRevB.35.6597
+
+    Args:
+        L: Left detector total counts.
+        R: Right detector total counts.
+        l: Left detector background counts.
+        r: Right detector background counts.
+        alpha: Ratio of detector counts.
+        beta: Ratio of detector asymmetries.
+
+    Returns:
+        The corrected asymmetry between two detectors.
     """
 
     A_raw = raw_asymmetry(L, R, l, r)
@@ -88,17 +162,38 @@ def corrected_asymmetry_error(
     L_error: float,
     R: float,
     R_error: float,
-    l: float = 0,
-    l_error=0,
-    r: float = 0,
-    r_error: float = 0,
-    alpha: float = 1,
-    alpha_error: float = 0,
-    beta: float = 1,
-    beta_error: float = 0,
+    l: float = 0.0,
+    l_error: float = 0.0,
+    r: float = 0.0,
+    r_error: float = 0.0,
+    alpha: float = 1.0,
+    alpha_error: float = 0.0,
+    beta: float = 1.0,
+    beta_error: float = 0.0,
 ) -> float:
-    """
-    Error propagation for the corrected asymmetry calculation.
+    """Uncertainty in the corrected asymmetry between two detectors.
+
+    Calculate the uncertainty in the corrected asymmetry using standard error
+    propagation.
+
+    Note: the calculation assumes all uncertainties are uncorrelated.
+
+    Args:
+        L: Left detector total counts.
+        L_error: Left detector uncertainty.
+        R: Right detector total counts.
+        R_error: Right detector uncertainty.
+        l: Left detector background counts.
+        l_error: Left detector background uncertainty.
+        r: Right detector background counts.
+        r_error: Right detector background uncertainty.
+        alpha: Ratio of detector counts.
+        alpha_error: Uncertainty in the ratio of detector counts.
+        beta: Ratio of detector asymmetries.
+        beta_error: Uncertainty in the ratio of detector asymmetries.
+
+    Returns:
+        The uncertainty of the raw asymmetry.
     """
 
     A_raw = raw_asymmetry(L, R, l, r)
@@ -124,11 +219,19 @@ def skewed_gaussian_mean(
     sigma_m__us: float,
     sigma_p__us: float,
 ) -> float:
-    """
-    Mean of the skewed Gaussian distribution (as defined in musrfit).
+    """Mean of the skewed Gaussian distribution (as defined in musrfit).
 
     See also: https://lmu.web.psi.ch/docu/LEM_Memo/skewedGaussian/skewedGaussian.pdf
+
+    Args:
+        B_0_G: Peak field position (G).
+        sigma_m__us: Negative damping parameter (μs\ :sup:`-1`).
+        sigma_p__us: Positive damping parameter (μs\ :sup:`-1`).
+
+    Returns:
+        The mean of the skewed Gaussian distribution (G).
     """
+
     return B_0_G + np.sqrt(2.0 / np.pi) * (sigma_p__us - sigma_m__us) / gamma_mu
 
 
@@ -140,10 +243,20 @@ def skewed_gaussian_mean_error(
     sigma_p__us: float,
     sigma_p_error__us: float,
 ) -> float:
-    """
-    Uncertainty of the mean of the skewed Gaussian distribution (as defined in musrfit).
+    """Uncertainty of the mean of the skewed Gaussian distribution (as defined in musrfit).
 
     See also: https://lmu.web.psi.ch/docu/LEM_Memo/skewedGaussian/skewedGaussian.pdf
+
+    Args:
+        B_0_G: Peak field position (G).
+        B_0_error_G: Uncertainty in the peak field position (G).
+        sigma_m__us: Negative damping parameter (μs\ :sup:`-1`).
+        sigma_m_error__us: Uncertainty in the negative damping parameter (μs\ :sup:`-1`).
+        sigma_p__us: Positive damping parameter (μs\ :sup:`-1`).
+        sigma_p_error__us: Uncertainty in the positive damping parameter (μs\ :sup:`-1`).
+
+    Returns:
+        The uncertainty in the mean of the skewed Gaussian distribution (G).
     """
 
     # convert the units of the sigmas from 1/us to G
